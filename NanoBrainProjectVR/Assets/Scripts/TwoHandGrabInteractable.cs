a617d64664260bfd4fe0e511907391de0ba4d51c
Şeref Şakacı <serefsakaci98@gmail.com>
2026-04-28 09:27:12 +0300

    Update TwoHandGrabInteractable.cs

--- NanoBrainProjectVR/Assets/Scripts/TwoHandGrabInteractable.cs
+++ NanoBrainProjectVR/Assets/Scripts/TwoHandGrabInteractable.cs
@@ -100,7 +100,7 @@ public class TwoHandGrabInteractable : XRGrabInteractable
                         rb.MovePosition(finalPos);
                         rb.MoveRotation(finalRot);
                         
-                        rb.velocity = Vector3.zero;
+                        rb.linearVelocity = Vector3.zero;
                         rb.angularVelocity = Vector3.zero;
                     }
                 }
