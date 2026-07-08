d18415cb00cf40a079f1d58df9228a7cca027ee7
Serclarus <serefsakaci98@gmail.com>
2026-07-09 01:10:45 +0300

    updt

--- NanoBrainProjectVR/Assets/Scripts/AmmoPouch.cs
+++ NanoBrainProjectVR/Assets/Scripts/AmmoPouch.cs
@@ -38,6 +38,10 @@ public class AmmoPouch : MonoBehaviour, IXRSelectFilter
         socketInteractor = GetComponent<XRSocketInteractor>();
         if (socketInteractor != null)
         {
+            // FORCE disable XRI's native fake meshes to prevent floating offset visuals!
+            socketInteractor.showInteractableHoverMeshes = false;
+            socketInteractor.showInteractableSelectionMeshes = false;
+
             socketInteractor.selectExited.AddListener(OnItemRemovedFromSocket);
             // Add ourselves as a filter so we can reject native trigger grabs (like the Shotgun loading port!)
             socketInteractor.selectFilters.Add(this);
