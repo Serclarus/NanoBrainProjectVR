2a7a1801d7e0299e3683a305b0fa8329eb76b88e
Serclarus <serefsakaci98@gmail.com>
2026-07-13 04:08:17 +0300

    Update NetworkPlayerLoadout.cs

--- NanoBrainProjectVR/Assets/Scripts/NetworkPlayerLoadout.cs
+++ NanoBrainProjectVR/Assets/Scripts/NetworkPlayerLoadout.cs
@@ -10,15 +10,35 @@ public class NetworkPlayerLoadout : NetworkBehaviour
 
     public override void OnNetworkSpawn()
     {
-        // Only the player who owns this body gets to spawn their weapons!
-        if (IsOwner)
+        // ONLY the server is allowed to spawn Network Objects.
+        if (IsServer)
         {
-            SpawnWeaponsServerRpc(OwnerClientId);
+            // Spawn weapons immediately when the player joins
+            SpawnWeaponsForClient(OwnerClientId);
+            
+            // Subscribe to scene load events so we can respawn weapons when the map changes!
+            if (NetworkManager.Singleton.SceneManager != null)
+            {
+                NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += OnSceneLoaded;
+            }
         }
     }
 
-    [ServerRpc]
-    private void SpawnWeaponsServerRpc(ulong clientId)
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer && NetworkManager.Singleton != null && NetworkManager.Singleton.SceneManager != null)
+        {
+            NetworkManager.Singleton.SceneManager.OnLoadEventCompleted -= OnSceneLoaded;
+        }
+    }
+
+    private void OnSceneLoaded(string sceneName, UnityEngine.SceneManagement.LoadSceneMode loadSceneMode, System.Collections.Generic.List<ulong> clientsCompleted, System.Collections.Generic.List<ulong> clientsTimedOut)
+    {
+        // When the server loads a new map, the old weapons were destroyed. Respawn them!
+        SpawnWeaponsForClient(OwnerClientId);
+    }
+
+    private void SpawnWeaponsForClient(ulong clientId)
     {
         // The server physically creates the weapons and hands ownership to the player
         if (shotgunPrefab != null) SpawnAndAssign(shotgunPrefab, clientId);
