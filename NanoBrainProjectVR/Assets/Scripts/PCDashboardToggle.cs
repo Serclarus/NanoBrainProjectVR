4e6b87e34ace452a8bffb6e22364e8a51dc5add5
Serclarus <serefsakaci98@gmail.com>
2026-07-18 14:40:17 +0300

    editorvr spectator disacticate

--- NanoBrainProjectVR/Assets/Scripts/PCDashboardToggle.cs
+++ NanoBrainProjectVR/Assets/Scripts/PCDashboardToggle.cs
@@ -3,18 +3,28 @@ using UnityEngine.XR;
 
 public class PCDashboardToggle : MonoBehaviour
 {
+    [Tooltip("If true, playing in the Unity Editor will act like a VR headset is plugged in, turning this UI OFF.")]
+    public bool forceVRInEditor = true;
+
     void Start()
     {
-        // XRSettings.isDeviceActive returns true ONLY if a physical VR headset is plugged in
-        // and currently active (running Oculus/SteamVR/OpenXR).
-        if (XRSettings.isDeviceActive)
+        // On Android (Quest), we are ALWAYS VR.
+        bool isAndroid = (Application.platform == RuntimePlatform.Android);
+        
+        // In the Unity Editor, we act as VR if forceVRInEditor is true.
+        bool isForcedEditorVR = (Application.isEditor && forceVRInEditor);
+        
+        // On PC, we check if a physical headset is actually running.
+        bool isRealVR = XRSettings.isDeviceActive;
+
+        if (isAndroid || isForcedEditorVR || isRealVR)
         {
-            Debug.Log("<color=green>[PCDashboardToggle]</color> VR Headset detected! Disabling the PC Operator Dashboard UI.");
+            Debug.Log("<color=green>[PCDashboardToggle]</color> VR Mode active! Disabling the PC Operator Dashboard UI.");
             gameObject.SetActive(false);
         }
         else
         {
-            Debug.Log("<color=cyan>[PCDashboardToggle]</color> No VR Headset detected. Activating PC Operator Dashboard UI.");
+            Debug.Log("<color=cyan>[PCDashboardToggle]</color> PC Operator Mode active. Activating PC Operator Dashboard UI.");
             gameObject.SetActive(true);
         }
     }
